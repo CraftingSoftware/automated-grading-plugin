708d2e09f62e494109d89a6562d8fd388d15cfe9
BillOchieng <ochieng01@allegheny.edu>
2022-11-03 18:47:53 -0400

    added few files

---
+++ sample_data/push_bulk.sh
@@ -0,0 +1,26 @@
+
+# Bulk Pusher script.
+# This script uses the File, dirNames, to locate repositories to push
+# The current date is printed in the commit message of the submit
+# A file, "0_thisLastPush.txt" is created to state when the last bulk push was completed.
+
+
+NOW=`date`
+printf "Current date and time in Linux is: $NOW"
+
+date > 0_thisLastPush.txt
+
+pwd > mydir
+for z in `cat mydir`; do cd $z; done
+for DIRNAME in $(cat dirNames)
+do
+    cd $DIRNAME
+    echo Checking: $DIRNAME
+     git add -A
+     git commit -m "Grade update: $NOW"
+     git push
+    cd $z/
+done
+
+rm mydir
+Footer
