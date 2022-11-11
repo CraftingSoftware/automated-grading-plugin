2fea52325f2949a2f296fa6881c9e6164afeae8b
Nic Ingerson <ingersonmeacham01@allegheny.edu>
2022-11-11 00:00:19 -0500

    Fix tabulation for TODO markers

--- automatic-grading/automated_grading.py
+++ automatic-grading/automated_grading.py
@@ -1,7 +1,19 @@
+#!
 """Automate grading using SheetShuttle."""
 
+import numpy as np
+import pandas as pd
 from sheetshuttle import github_interaction, sheet_collector, util
 import yaml
+
+STANDARD_REPO_NAME = "CMPSC-203-Allegheny-College-Fall-2022/automated-grading-plugin"
+STANDARD_TITLE = "Automated Grading Plugin"
+STANDARD_BODY = "Your final grade is:"
+STANDARD_LABELS = ["SheetShuttle", "Automated Grading"]
+CONFIG_WRITE_DIR = "config/"
+CONFIG_WRITE_FILENAME = "automated_grading_gh_config.yml"
+
+# TODO: Implement function to run the sheets collector
 
 class Student:
     def __init__(self, name: str, email: str, username: str) -> None:
@@ -20,9 +32,9 @@ class Student:
         }
         
         
-    # TODO: Implement functions to add grades for the following:
-    # engineering efforts, projects, surveys, exams, and participation
-    
-    # TODO: Implement function to retrieve grades
-    
-    # TODO: Implement function to send grades to GitHub
+# TODO: Implement functions to add grades for the following:
+# engineering efforts, projects, surveys, exams, and participation
+
+# TODO: Implement function to retrieve grades
+
+# TODO: Implement function to send grades to GitHub
