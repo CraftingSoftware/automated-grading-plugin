e07cc350b28977e815bae80a2002358f6f54d0ed
Nic Ingerson <ingersonmeacham01@allegheny.edu>
2022-12-01 13:37:24 -0500

    Add functions and TODO markers in automated_grading.py

--- automatic-grading/automated_grading.py
+++ automatic-grading/automated_grading.py
@@ -1,40 +1,18 @@
-#!
-"""Automate grading using SheetShuttle."""
+from sheetshuttle import sheet_collector
+from sheetshuttle import util
+from sheetshuttle import github_interaction
+from sheetshuttle import github_objects
 
-import numpy as np
-import pandas as pd
-from sheetshuttle import github_interaction, sheet_collector, util
 import yaml
 
-STANDARD_REPO_NAME = "CMPSC-203-Allegheny-College-Fall-2022/automated-grading-plugin"
-STANDARD_TITLE = "Automated Grading Plugin"
-STANDARD_BODY = "Your final grade is:"
-STANDARD_LABELS = ["SheetShuttle", "Automated Grading"]
-CONFIG_WRITE_DIR = "config/"
-CONFIG_WRITE_FILENAME = "automated_grading_gh_config.yml"
-
-# TODO: Implement function to run the sheets collector
-
-class Student:
-    def __init__(self, name: str, email: str, username: str) -> None:
-        """Define a student's name, email, username, and their grades."""
-        # referenced from SheetShuttle's ee_grades.py sample plugin
-        self.name = name
-        self.email = email
-        self.username = username
-        # define grade values for each student
-        self.grades = {
-            "ee": {"num": 0, "sum": 0, "weight": 0.4},
-            "projects": {"num": 0, "sum": 0, "weight": 0.15},
-            "surveys": {"num": 0, "sum": 0, "weight": 0.15},
-            "exams": [],
-            "participation": {"grade": 0, "weight": 0.05},
-        }
-        
-        
-# TODO: Implement functions to add grades for the following:
-# engineering efforts, projects, surveys, exams, and participation
-
-# TODO: Implement function to retrieve grades
-
-# TODO: Implement function to send grades to GitHub
+def extract_sheet_data():
+    """Extract the sheet data and dump it into a config file."""
+    sheets_id = input("Input Google Sheets ID: ")
+    # TODO: dump ID into a YAML file
+    # reference for YAML format: SheetShuttle/config/sheet_sources/sample_config.yml
+    sheet_collector.collect_files(
+        # TODO: add YAML file created as input for this method
+        )
+    # TODO: collect region data with respect to the individual student
+    # TODO: get collected regions using sheet_collector.get_regions() method
+    # TODO:
