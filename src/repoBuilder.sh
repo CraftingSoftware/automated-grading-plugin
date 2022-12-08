64258d59251836f77feaef48ee912302171a61ed
Brock Brumfield <brumfieldb@allegheny.edu>
2022-12-08 11:48:22 -0500

    new file added

--- src/repoBuilder.sh
+++
@@ -1,6 +0,0 @@
-# add the git clone statements here to clone the gradebook repositories. Once the repositories have been cloned, they are copied into a student_repos directory. This file can be edited as necessary.
-
-
-mkdir studentGradeBook_Repos
-mv gradebook* studentGradeBook_Repos/
-
