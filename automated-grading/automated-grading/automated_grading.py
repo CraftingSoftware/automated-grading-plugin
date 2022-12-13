afeaade3fcbc1a43cd0087c6a8f3efdee4a60af2
Brock Brumfield <brumfieldb@allegheny.edu>
2022-12-13 13:02:43 -0500

    automated grading updated

--- automated-grading/automated-grading/automated_grading.py
+++ automated-grading/automated-grading/automated_grading.py
@@ -1,115 +1,63 @@
-"""This file when put into `sheetshuttle/plugins`, can run to display the student grades whenever an update is made."""
-from sheetshuttle import sheet_collector
-from sheetshuttle import util
-from sheetshuttle import github_interaction
-from sheetshuttle import github_objects
+from __future__ import print_function
 
-import yaml
+import os.path
 
-# file that has authentication info, don't push `"n_key.json"` to Github!
-key_file = "n_key.json"
-# Directory containing config info for our sheets
-src_directory = "config/sheet_sources"
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-def run(sheets_keys_file, sheets_config_directory, **kwargs):
-    """This function collects and fetches information from google sheets."""
-    # has_credentials = google_credentials(key_file)
-    # if has_credentials:
-    #     raise Exception(f"ERROR: No credentials found in {key_file}")
-    collector = sheet_collector.SheetCollector(key_file, src_directory)
-    # sheet collector object called collector with args key_file and src_dir
-    collector.collect_files()
-    old_data_dict = {}
-    # TODO: collect region data with respect to the individual student
-    for region_data in collector.sheets_data["config"].regions:
-        print(region_data.data)
-        # TODO:iterate through diff regions
-        # covert region data into a dictionary so as to assign keys and values
-        my_data = region_data.data
-        new_data_dict = my_data.to_dict()
-        old_data_dict.update(new_data_dict)
-        for row_count in range(len(new_data_dict["Name"])):
-            if row_count == 0:
-                master = make_dict(
-                    new_data_dict["S_Name"][row_count],
-                    new_data_dict["GH"][row_count],
-                    new_data_dict["Prof_Name"][row_count],
-                    new_data_dict["Prof_GH"][row_count],
-                    new_data_dict["Total"][row_count],
-                )
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
 
-def make_dict(Student_Name, studentgh, ProfName, ProfGH, Points):
-    the_dict = {
-        Student_Name: {
-            "GH": studentgh,
-            "Prof_Name": ProfName,
-            "Prof_GH": ProfGH,
-            "Total": Points,
-        },
-    }
-    return the_dict
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+    except HttpError as err:
+        print(err)
 
 
-def add_info_dict(master, Student_Name, studentgh, ProfName, ProfGH, Points):
-    the_dict = make_dict(Student_Name, studentgh, ProfName, ProfGH, Points)
-    master.update(the_dict)
-    return the_dict
-
-
-def add_dict_to_dict(master, the_dict):
-    master.update(the_dict)
-    return the_dict
-
-<<<<<<< HEAD
-# def gh_verification(username, access_token):
-=======
-<<<<<<< HEAD
-def gh_verification(username, access_token):
-=======
-    # TODO: Start debugging
-
-
-def gh_verification():
->>>>>>> 433ff84e9bf1ce15752039ee0559f39c90beebfd
->>>>>>> 2c3f2db45de9315e1f3c3f8c21f4f61ec6a3366a
-    """Verify permissions for GitHub interaction."""
-  # Set up the API endpoint and authorization headers
-      # TODO: check github token
-  # headers = {
-   # "Authorization": "Bearer {}".format(access_token)
-   # }
-
-    # Send a GET request to the API endpoint to retrieve the user's repositories
-    # response = requests.get(endpoint, headers=headers)
-
-    # Check if the request was successful and the user has permission to interact with GitHub
-    # if response.status_code == 200 and response.json()[0]["owner"]["login"] == username:
-    #    return True
-    # else:
-    #    return False
-    # TODO: calculate dimensions of regions and fill empty cells
-    # TODO: remove this line after completion:
-    # return None
-
-def gh_pushfile():
-    """Push markdown files to individual students' repositories."""
-    # TODO: parse through the config file
-    # TODO: check if the markdown file already exists in the repo
-    # TODO: add files as input to the following functions
-    if github_objects.exists():
-        github_objects.update_file()
-    else:
-        github_objects.create_file()
-    github_objects.post()  # post the file to GitHub repository
-    return None
-
-
-if __name__ == "__main__":
-    # Call procedures
-    extract_sheet_data()
-    gh_verification()
-    gh_pushfile()
-
-# sheetshuttle -pd /Users/rawlings/Desktop/Software class/automated-grading-plugin/automated-grading/automated-grading -pn automated_grading
+if __name__ == '__main__':
+    main()
