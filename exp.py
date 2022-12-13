14160b18e4b6eca08ff433c86c72c53ace2be5fd
Aveet Desai <desaia@allegheny.edu>
2022-12-13 14:44:11 -0500

    added code

---
+++ exp.py
@@ -0,0 +1,30 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+# generate all the proper credentials needed to fetch info
+SERVICE_ACCOUNT_FILE = 'n_key.json'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# The ID sample spreadsheet we want to read and write to
+SAMPLE_SPREADSHEET_ID = '1vbO2uR_nJv15Ev5LrcVaBl2FY0A37b3CHj1ABrQhaFA'
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range="Sheet1!A1:AM38").execute()
+values = result.get('values', [])
+
+for row in values:
+  # Print the row, with each column separated by a comma
+  print(','.join(row))
+
+
+
+
+
