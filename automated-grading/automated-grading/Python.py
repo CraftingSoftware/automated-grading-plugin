2c3f2db45de9315e1f3c3f8c21f4f61ec6a3366a
BillOchieng <ochieng01@allegheny.edu>
2022-12-13 12:38:12 -0500

    debug run function

---
+++ automated-grading/automated-grading/Python.py
@@ -0,0 +1,15 @@
+""""Default plugin for SheetShuttle."""
+
+from sheetshuttle import github_objects
+from sheetshuttle import sheet_collector
+
+from github import Github
+
+# import os
+
+
+def run(sheets_keys_file, sheets_config_directory, gh_config_directory, **kwargs):
+    """Standard run function."""
+    print("hello from the default plugin")
+    print("Additional arguments")
+    print(kwargs["args"])
